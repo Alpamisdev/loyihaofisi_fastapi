6bb5cf3bc3eae6011ecc1932ac0751914e395954
Alpamis <sbsbillion1@gmail.com>
2025-04-29 22:56:33 +0500

    fixed run

--- app/main.py
+++ app/main.py
@@ -162,4 +162,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Run the application
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=5001, reload=True)
